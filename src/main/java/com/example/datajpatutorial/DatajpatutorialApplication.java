f2920ac81d2829d61bea5034c966e6c24cb3504e
unknown <graham.flaspoehler@paradyme.us>
2020-05-14 20:29:43 -0400

    First Commit

---
+++ src/main/java/com/example/datajpatutorial/DatajpatutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.datajpatutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatajpatutorialApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DatajpatutorialApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/datajpatutorial/DatajpatutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datajpatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatajpatutorialApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
